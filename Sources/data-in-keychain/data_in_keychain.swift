0b0aa9afb7d4b326d68c3fda3213b7ddc7b3a8d4
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2023-03-08 17:15:42 +0100

    Added methods to `Data` to facilitate persistence in Keychain.

--- Sources/data-in-keychain/data_in_keychain.swift
+++ Sources/data-in-keychain/data_in_keychain.swift
@@ -1,6 +1,142 @@
-public struct data_in_keychain {
-    public private(set) var text = "Hello, World!"
+//
+//  data_in_keychain.swift
+//  
+//
+//  Created by Jeremy Bannister on 3/8/23.
+//
 
-    public init() {
+///
+@_exported import OSErrorModule
+@_exported import LocalAuthentication
+
+
+///
+public extension Data {
+    
+    ///
+    @MainActor
+    static func fetchedFromKeychain
+        (uniqueKey: String,
+         authenticationContext: LAContext? = nil)
+    throws -> Self? {
+        
+        ///
+        var query = [kSecClass: kSecClassGenericPassword,
+                     kSecAttrAccount: uniqueKey,
+                     kSecReturnData: true] as [String: Any]
+        
+        ///
+        #if os(iOS)
+        query[kSecUseDataProtectionKeychain as String] = true
+        #endif
+        
+        ///
+        if let authenticationContext = authenticationContext {
+            query[kSecUseAuthenticationContext as String] = authenticationContext
+        }
+
+        ///
+        var item: CFTypeRef?
+        
+        ///
+        let error: OSError? =
+            SecItemCopyMatching(
+                query as CFDictionary,
+                &item
+            )
+                .asOSError
+        
+        ///
+        if let error {
+            
+            ///
+            if case .errSecItemNotFound = error {
+                
+                ///
+                return nil
+                
+            } else {
+                
+                ///
+                throw error
+            }
+            
+        /// Nil error means success
+        } else {
+            
+            ///
+            return item as? Data
+        }
+    }
+    
+    ///
+    @MainActor
+    func saveToKeychain
+        (uniqueKey: String,
+         accessGroup: String,
+         requireBiometry: Bool)
+    throws {
+        
+        ///
+        var query: [String: Any] =
+            [
+                kSecClass: kSecClassGenericPassword,
+                kSecAttrAccount: uniqueKey,
+                kSecUseDataProtectionKeychain: true,
+                kSecAttrAccessGroup: accessGroup,
+                kSecValueData: self
+            ] as [String: Any]
+        
+        ///
+        if requireBiometry {
+            
+            ///
+            query[kSecAttrAccessControl as String] =
+                SecAccessControlCreateWithFlags(
+                    nil,
+                    kSecAttrAccessibleWhenPasscodeSetThisDeviceOnly,
+                    .biometryCurrentSet,
+                    nil
+                )
+        } else {
+            
+            ///
+            query[kSecAttrAccessible as String] = kSecAttrAccessibleWhenPasscodeSetThisDeviceOnly
+        }
+
+        ///
+        let error: OSError? =
+            SecItemAdd(
+                query as CFDictionary,
+                nil
+            )
+                .asOSError
+        
+        ///
+        if let error { throw error }
+    }
+    
+    ///
+    @MainActor
+    static func deleteFromKeychain (uniqueKey: String) throws {
+        
+        ///
+        var query = [kSecClass: kSecClassGenericPassword,
+                     kSecAttrAccount: uniqueKey] as [String: Any]
+        
+        ///
+        #if os(iOS)
+        query[kSecUseDataProtectionKeychain as String] = true
+        #endif
+        
+        ///
+        let error: OSError? =
+            SecItemDelete(
+                query as CFDictionary
+            )
+                .asOSError
+        
+        ///
+        if let error { throw error }
     }
 }
